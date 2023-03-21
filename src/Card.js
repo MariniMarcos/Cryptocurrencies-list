6a53f302f646a1140eec46bc475d5bd970691b06
Marini Marcos <mmarini95@hotmail.com>
2023-03-20 23:06:27 -0300

    lala

--- src/Card.js
+++ src/Card.js
@@ -16,14 +16,14 @@ const Card = ({ crypto }) => {
                 <p class={`percent ${crypto['price_change_percentage_24h'] < 0 ? 'negative' : ''}`} style={{ overflow: 'visible' }}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1792 1792" fill="currentColor" height="20" width="20" class={`${crypto['price_change_percentage_24h'] < 0 ? 'rotate-180' : ''}`}>
                         <path d={`M1408 576q0-26-19-45l-448-448q-19-19-45-19t-45 19l-448 448q-19 19-19 45 0 26 19 45t45 19h896q26 0 45-19t19-45z`} fill={crypto['price_change_percentage_24h'] < 0 ? 'red' : 'green'}></path>
-                    </svg> {crypto['price_change_percentage_24h']} %
+                    </svg> {crypto['price_change_percentage_24h'].toFixed(2)} %
                 </p>
             </div>
             <div class="data">
                 <p>
-                    {crypto["current_price"]} USD
+                    {crypto["current_price"].toFixed(2)} USD
                 <br />    
-                    {crypto["current_price"]*380} ARG
+                    {crypto["current_price"].toFixed(2)} ARG
                 </p>
             </div>
         </div>
