decc732403d096cec1ad0dcdb1cccd5a0258764a
Marini Marcos <mmarini95@hotmail.com>
2023-04-28 20:42:50 -0300

    agregue loco y set interval para actualizacion

--- src/CryptoListContainer.js
+++ src/CryptoListContainer.js
@@ -1,32 +1,43 @@
-import React from 'react'
-import CryptoList from './CryptoList'
-import { useEffect, useState } from 'react';
-
-
+import React, { useEffect, useState } from 'react';
+import CryptoList from './CryptoList';
 
 const CryptoListContainer = () => {
-    const [prices, setPrices] = useState([]);
-    const [searchTerm, setSearchTerm] = useState('');
+  const [prices, setPrices] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
-    useEffect(() => {
-        const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
-         fetch(url)
-            .then((response) => response.json())
-            .then((data) => setPrices(data))
-            .catch((error) => console.log(error));
-    }, []);
+  useEffect(() => {
+    const url =
+      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
 
+    const fetchPrices = () => {
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => setPrices(data))
+        .catch((error) => console.log(error));
+    };
 
-    const filteredCryptos = prices.filter((cryptos) => {
-        return   cryptos["symbol"].includes(searchTerm);
-    });
+    // Fetch prices initially
+    fetchPrices();
 
-    return (
-        <div className='CryptoListContainer'>
-            <h1>Mercado Crypto</h1>
-            <CryptoList cryptos={filteredCryptos}/>
-        </div>
-    )
-}
+    // Fetch prices every 5 minutes
+    const intervalId = setInterval(() => {
+      fetchPrices();
+    }, 300000); // 5 minutes in milliseconds
 
-export default CryptoListContainer
+    // Cleanup function to clear the interval
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const filteredCryptos = prices.filter((crypto) => {
+    return crypto['symbol'].includes(searchTerm);
+  });
+
+  return (
+    <div className='CryptoListContainer'>
+      <h1>Mercado Crypto</h1>
+      <CryptoList cryptos={filteredCryptos} />
+    </div>
+  );
+};
+
+export default CryptoListContainer;
